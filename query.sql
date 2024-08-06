0c5773e19bb029d5695417dcda922fcf51d2ec73
Farrukh <adler4313@gmail.com>
2024-08-06 13:57:08 +0500

    Todo

--- query.sql
+++ query.sql
@@ -1,26 +1,27 @@
-CREATE DATABASE todo_app;
+CREATE
+DATABASE todo_app;
 
-USE todo_app;
+USE
+todo_app;
 
-CREATE TABLE users (
-
-                       id INT AUTO_INCREMENT PRIMARY KEY,
-                       chat_id BIGINT,
-                       status VARCHAR(32)
- addtask      varchar(32)  | YES  |     | NULL    |                |
- gettask      varchar(32) ,
- checktask    varchar(32) ,
- unchecktask  varchar(32) ,
- deletetask   varchar(32) ,
- edittask     varchar(32) ,
- email        varchar(255),
- password
+CREATE TABLE users
+(
+    id          INT AUTO_INCREMENT PRIMARY KEY,
+    addtask     varchar(32),
+    gettask     varchar(32),
+    checktask   varchar(32),
+    unchecktask varchar(32),
+    deletetask  varchar(32),
+    edittask    varchar(32),
+    email       varchar(255),
+    password    varchar(255)
 );
 
-CREATE TABLE todos (
+CREATE TABLE todos
+(
 
-                       id INT AUTO_INCREMENT PRIMARY KEY,
-                       user_id INT,
-                       text VARCHAR(255),
-                       status tinyint(1)
+    id      INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    text    VARCHAR(255),
+    status  tinyint(1)
 );
